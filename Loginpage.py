46ab06376a32ff473736f71005077646f7239ee1
Njui Njeri <snjui7@gmail.com>
2018-12-12 14:01:27 +0300

    readme.md.txt git status git commit -m readme.md.txt

---
+++ Loginpage.py
@@ -0,0 +1,17 @@
+class login:
+    def __init__(self,id,pas):
+        self.id="admin"
+        self.pas="admin"
+
+    def check(id,pas):
+        print self.id
+        print lod.id
+        if(self.id==log.id and self.pas==log.pas):
+            print "Login success!"
+
+
+log=login("","")
+log.check(raw_input("Enter Login ID:"),
+        input("Enter password: "))
+
+print "Login Page"
